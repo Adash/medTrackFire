9a0389c051e1b5aee215ddac43b2e0c92c570bb8
Adash <adash108@yahoo.com>
2019-05-31 16:44:48 +0100

    removed round corners on all elements via global override

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './app.css' // global css override
 
 import HomePage from '../Home';
 import LandingPage from '../Landing';
