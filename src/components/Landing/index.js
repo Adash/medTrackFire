6edf09a55304a44427e915817a72ce36f69d6859
Adash <adash108@yahoo.com>
2019-06-05 20:28:37 +0100

    added styling to landing page - work in progress

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-//import './landing.css';
+import './landing.css';
 import SignInForm from '../SignIn/index'
 
 const Landing = () => (
   <div className="landing_page">
-    <h2>Log in</h2>
-    <SignInForm />
+    <div className="logo-box">
+      MedTrack<span style={{color:"yellow"}}>Fire</span>
+    </div>
+    <div className="signinform-container">
+      <SignInForm />
+    </div>
   </div>
 );
 
