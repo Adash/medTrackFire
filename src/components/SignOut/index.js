652058015dd0e9164a6cd30a1f360ed25d9f6ec3
Adash <adash108@yahoo.com>
2019-06-07 16:46:20 +0100

    added redirect to landing page on sign out

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 
 import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
 
-const SignOutButton = ({firebase}) => (
-  <Link
-    to="#"
-    className="nav-link"
-    onClick={ firebase.fbSignOut }
-  >
-    Sign Out
-  </Link>
-)
+const SignOutButton = ({firebase}) => {
+  const [redirect, setRedirect] = useState(false);
+
+  const signOut = () => {
+    firebase.fbSignOut()
+      .then(setRedirect(true))
+      .catch(error => console.log(error))
+  }
+
+  return (
+    <>
+      { redirect && <Redirect to={ROUTES.LANDING} /> }
+      <Link
+        to="#"
+        className="nav-link"
+        onClick={ signOut }
+      >
+        Sign Out
+      </Link>
+    </>
+  )}
 
 export default withFirebase(SignOutButton);
