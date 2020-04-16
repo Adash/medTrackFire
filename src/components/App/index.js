6b98615262a59d0dbdebc0c77900f4f1d790d1cd
Adam <adash108@yahoo.com>
2020-04-16 17:42:56 +0100

    using bootstrap as an import instead of fetching it in html file

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { withAuthentication } from '../Session'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css' // global css override
 
 import HomePage from '../Home';
